2448c5a8dfd36c775e85c9a13490a9dbbc075e27
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-06-04 14:08:43 +0400

    Static Generation

--- pages/index.js
+++ pages/index.js
@@ -67,13 +67,12 @@ export default function Home() {
     )
 }
 */
-
-
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import {getSortedPostsData} from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
     return (
         <Layout home>
             <Head>
@@ -86,6 +85,51 @@ export default function Home() {
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
             </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Layout>
     )
+}
+
+/*export async function getStaticProps() {
+    // Get external data from the file system, API, DB, etc.
+    // const res = await fetch(`https://.../data`);
+    // const data = await res.json()
+    const data = [1,2,3,4,5];
+
+    if (!data) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+    // The value of the `props` key will be
+    //  passed to the `Home` component
+    return {
+        props: {data}
+    }
+}*/
+
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
