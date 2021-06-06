ef861df62232daa8728f4115690ef22ba7b0a0b0
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-06-06 11:20:19 +0400

    git repos add and update Vercel

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ export default function Home({allPostsData}) {
                     <p>[Your Self Introduction]</p>
                     <p>
                         (This is a sample website - you’ll be building a site like this on{' '}
-                        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+                        <a href="https://nextjs.org/learn">Next.js tutorial in nextjs.org website</a>.)
                     </p>
 
                 </section>
